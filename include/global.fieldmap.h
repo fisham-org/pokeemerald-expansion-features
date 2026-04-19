82ee4ac7bfe114b670db6446386d56809a449be8
fisham33 <fisham33@gmail.com>
2026-04-19 10:10:35 +1000

    expand metatiles to 2048 - part 1

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -1,14 +1,17 @@
 #ifndef GUARD_GLOBAL_FIELDMAP_H
 #define GUARD_GLOBAL_FIELDMAP_H
 
+// note: metatile total limit has been changed from 1024 to 2048 per instructions in https://github.com/pret/pokeemerald/wiki/Expanding-The-Metatile-Count
+
 // Masks/shifts for blocks in the map grid
-// Map grid blocks consist of a 10 bit metatile id, a 2 bit collision value, and a 4 bit elevation value
+// Map grid blocks consist of an 11 bit metatile id, a 1 bit collision value, and a 4 bit elevation value
 // This is the data stored in each data/layouts/*/map.bin file
-#define MAPGRID_METATILE_ID_MASK 0x03FF // Bits 0-9
-#define MAPGRID_COLLISION_MASK   0x0C00 // Bits 10-11
+// Originally a 10 bit metatile id and 2 bit collision; one collision bit was reclaimed to double the metatile id space.
+#define MAPGRID_METATILE_ID_MASK 0x07FF // Bits 0-10
+#define MAPGRID_COLLISION_MASK   0x0800 // Bit 11
 #define MAPGRID_ELEVATION_MASK   0xF000 // Bits 12-15
 #define MAPGRID_METATILE_ID_SHIFT 0
-#define MAPGRID_COLLISION_SHIFT  10
+#define MAPGRID_COLLISION_SHIFT  11
 #define MAPGRID_ELEVATION_SHIFT  12
 
 enum
